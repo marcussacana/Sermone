7a0ca03fdc16849fa5e1f353dcc11d7e4175aee8
Marcus André <ddtank.marcus@gmail.com>
2020-05-14 22:19:09 -0300

    Add Dependencies

---
+++ Dependencies/BlazorWorker/CoreInstanceService/ICoreInstanceService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace BlazorWorker.Core.CoreInstanceService
+{
+    public interface ICoreInstanceService
+    {
+        Task<IInstanceHandle> CreateInstance(Type type);
+        Task<IInstanceHandle> CreateInstance<T>();
+    }
+
+    public interface IInstanceHandle : IAsyncDisposable {
+    }
+}
